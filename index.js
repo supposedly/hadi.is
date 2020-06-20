034b51a14d6012782c2844f3cbf7ee19d06cfdc3
ht <eltrhn@gmail.com>
2020-06-20 01:30:00 -0700

    Rework layout with ampersand, create index.js, add some responsivity

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+(function() {
+  window.addEventListener('load', init);
+
+  function init() {
+    document.getElementById('expand').addEventListener('click', showRest);
+  }
+
+  function showRest() {
+    
+  }
+})();
