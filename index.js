7e0283c8347231a67f925560f6d8bf575e75c6c3
ht <eltrhn@gmail.com>
2020-06-22 02:05:24 -0700

    Create navbutton.html and index.scss, get scrolling working on main page

--- index.js
+++ index.js
@@ -3,10 +3,37 @@
   window.addEventListener('load', init);
 
   function init() {
-    document.getElementById('expand').addEventListener('click', showRest);
+    document.getElementById('show-links').addEventListener('click', showLinks);
+    document.getElementById('show-main').addEventListener('click', showMain);
   }
 
-  function showRest() {
-    
+  function showLinks() {
+    document.getElementById('links').classList.remove('hidden');
+    document.getElementById('links').scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
+    awaitScrollEnd(
+      () => document.getElementById('main').classList.add('hidden')
+    );
+  }
+
+  function showMain() {
+    document.getElementById('main').classList.remove('hidden');
+    document.getElementById('links').scrollIntoView(true);
+    setTimeout(() => window.scrollTo({top: 0, behavior: 'smooth'}), 1);
+    awaitScrollEnd(
+      () => document.getElementById('links').classList.add('hidden')
+    );
+  }
+
+  // thanks https://stackoverflow.com/a/51142522/
+  function awaitScrollEnd(callback) {
+    let timeout;
+    function run() {
+      clearTimeout(timeout);
+      timeout = setTimeout(function() {
+        callback();
+        window.removeEventListener('scroll', run);
+      }, 100);
+    }
+    window.addEventListener('scroll', run);
   }
 })();
