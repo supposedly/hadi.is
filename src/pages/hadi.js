ca434e6e4658241e589f4939b461c811858a6de2
ht <eltrhn@gmail.com>
2020-07-08 02:01:37 -0700

    Missed one

--- src/pages/hadi.js
+++ src/pages/hadi.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby";
 import Layout from "../components/layout";
 
 export default () => (
-  <Layout title="hadi">
+  <Layout title="Hadi">
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
