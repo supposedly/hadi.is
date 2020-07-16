0238cd43028a1fc520b52dd7fa765ca948e15bbf
ht <eltrhn@gmail.com>
2020-07-16 01:48:50 -0700

    Finally create placeholder blog/resume pages to stop the 404 prefetch errors

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import Layout from "../components/layout";
+
+export default () => (
+  <Layout title="blog">
+    <h1>Nothing here yet</h1>
+    <p>Brew a coffee and come back</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
