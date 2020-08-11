79a28c679a88e4970595454f4149c0988f3e26d0
ht <eltrhn@gmail.com>
2020-08-11 01:57:23 -0700

    Fix homepage and stickbug loop

--- src/pages/stickbugged/lol.js
+++ src/pages/stickbugged/lol.js
@@ -1,17 +1,15 @@
 import React from "react";
 
-import "../../styles/stickbug.css";
-
 export default () => (
-  <div class="container" style={{ height: '100%' }}>
+  <div className="container" style={{ position: 'absolute', height: '100%' }}>
     <iframe
       title="Get stick bugged lol"
       width="100%"
       height="100%"
       src="https://www.youtube-nocookie.com/embed/fC7oUOUEEi4?controls=0&autoplay=1&loop=1&playlist=fC7oUOUEEi4"
-      frameborder="0"
+      frameBorder="0"
       allow="accelerometer; autoplay; loop; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen>
+      allowFullScreen>
     </iframe>
   </div>
 )
